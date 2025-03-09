5419bb0ce9ea93bf2c13dccc7f9e1cb301f0d266
d_flex <hiyoo95@gmail.com>
2025-03-09 18:35:36 +1000

    REMS-13 : editted authController.js to include role field when creating user in registerUser controller function

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,12 +8,16 @@ const generateToken = (id) => {
 };
 
 const registerUser = async (req, res) => {
+
+    // Roles added for user authorization
     const { name, email, password, role } = req.body;
     try {
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
 
         const user = await User.create({ name, email, password, role });
+
+        // include role to create new user 
         res.status(201).json({ id: user.id, name: user.name, email: user.email, role: user.role, token: generateToken(user.id) });
     } catch (error) {
         res.status(500).json({ message: error.message });
